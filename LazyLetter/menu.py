ae778dceb700e07f651e818906ac70bc25f2ab96
Collin M <wingedillidan@gmail.com>
2015-12-19 19:34:46 -0500

    Starting the menu system

---
+++ LazyLetter/menu.py
@@ -0,0 +1,75 @@
+import os
+
+from . import coverletter
+
+
+def _list_options(options, pre_spaces=4):
+    result = ""
+    for i, option in enumerate(options):
+        result += ' '*pre_spaces + '[' + str(i+1) + '] ' + option + '\n'
+
+    return result
+
+
+def _filter_options(lower_options, letter):
+    """
+    Takes a lise of lower-cased options and a letter and returns back a list
+    of options that only contain said letter.
+    """
+    working_options = []
+
+    for option in lower_options:
+        if letter in option:
+            working_options.append(option)
+
+    return working_options
+
+
+def _parse_options(options, answer):
+    """
+    WIP - instead of a -2 return, maybe a tuple of the many options?
+
+    Takes in a list of options and a user response which can be either an
+    int or any combination of letters within a certain option, or options.
+
+    Returns the index-value of the matching option, -1 if nothing matches, or
+    -2 if too many things match.
+    """
+    if type(answer) == str:
+        # --- Remove case sensitivity issues ---
+        lower_options = [None] * len(options)
+
+        for i, option in enumerate(options):
+            lower_options[i] = str(i) + option.lower()
+
+        answer = answer.replace(' ', '').lower()
+        # --------------------------------------
+
+        for letter in answer:
+            lower_options = _filter_options(lower_options, letter)
+
+            if len(lower_options) == 0:
+                return -1
+
+        if len(lower_options) > 1:
+            return -2
+        else:
+            return int(lower_options[0][0])
+    else:
+        answer = int(answer)
+        answer -= 1  # user options list is 1-based
+
+        if answer >= 0 and answer < len(options):
+            return answer
+        else:
+            return -1
+
+
+def hub(config):
+    options = ['Generate Cover Letter', 'Settings', 'Exit']
+    welcome = str("Navigate through the various menus by entering the " +
+                  "option, or option number, below:"
+                  )
+    while True:
+        print(welcome)
+        print(_list_options(options))
