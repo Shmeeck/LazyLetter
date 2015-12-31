0d75f037311b49c719f77d27a445658783f0ce4d
Collin M <wingedillidan@gmail.com>
2015-12-31 00:28:14 -0500

    more verbose startup

--- LazyLetter.py
+++ LazyLetter.py
@@ -1,6 +1,12 @@
 from LazyLetter import menu
+from LazyLetter import utility
 from LazyLetter import configurator
 from LazyLetter.configurator import get_config as config
+
+continue_msg = False
+utility.clear_screen()
+
+print("Initializing setup...")
 
 # attempt to load a previously save config
 try:
@@ -8,6 +14,11 @@ try:
 except FileNotFoundError:
     print("No config was found, making one...")
     config().save(force=False)
+    continue_msg = True
+
+print("\nSetup complete!")
+
+if continue_msg:
     input(config().continue_msg)
 
 menu.hub_navigation()
